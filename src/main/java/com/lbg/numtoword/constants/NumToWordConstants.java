3a3f084f8a4e1bd0a20c8d4c3753c0e2b9434fb4
shrini <p.shrinivas.reddy@gmail.com>
2018-07-29 14:37:01 +0530

    add

---
+++ src/main/java/com/lbg/numtoword/constants/NumToWordConstants.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.lbg.numtoword.constants;
+
+/**
+ * @author Balu
+ *
+ */
+public class NumToWordConstants {
+	
+	public final static String[] DIGITS = { "", "one", "two", "three", "four",
+		"five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve",
+		"thirteen", "fourteen", "fifteen", "sixteen", "seventeen",
+		"eighteen", "nineteen" };
+	
+	public final static String[] MULTIPLE_OF_TENS = { "", "", "twenty",
+		"thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+	
+
+	public final static String[] MILESTONES = { "", " thousand", " million" };
+
+	public final static String HUNDERD_CONSTANT = "hundred";
+
+	public final static String ZERO = "zero";
+
+}
+
+
+
