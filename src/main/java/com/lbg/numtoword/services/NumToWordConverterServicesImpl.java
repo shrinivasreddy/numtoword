ce41f5ac60c4c26d519ad1d3e01a5cdf00b81dc8
shrinivasreddy <p.shrinivas.reddy@gmail.com>
2018-07-31 13:04:41 +0530

    modularising code

--- src/main/java/com/lbg/numtoword/services/NumToWordConverterServicesImpl.java
+++ src/main/java/com/lbg/numtoword/services/NumToWordConverterServicesImpl.java
@@ -16,10 +16,7 @@ public class NumToWordConverterServicesImpl implements
 	public String toWord(long num) throws ExceedRangeException {
 
 		// Validate input
-		if (num < -999_999_999 || num > 999_999_999) {
-			throw new ExceedRangeException(
-					"Invalid input! Give input between -999999999 to 999999999 inclusive.");
-		}
+		validateInput(num);
 
 		// handle zero
 		if (num == 0) {
@@ -30,29 +27,52 @@ public class NumToWordConverterServicesImpl implements
 
 		final int positiveNum = (int) (isNegative ? -(num) : num);
 
+		String[] partWords = "";
+		
 		// Divide the numbe into parts of three digits
-		int part = positiveNum % 1000;
-		int tempNum = positiveNum;
-		String words = "";
-		int index = 0;
-
-		do {
-			if (part > 0) {
+		int[] parts = NumToWordUtility.toThreeDigitParts(positiveNum);
+		for (int i=0; i < parts.size(); i++) {
+			if (parts[i] > 0) {
 				words = NumToWordUtility.toWordsFromThreeDigits(part) + NumToWordConstants.MILESTONES[index]
 						+ words;
-			}
-			tempNum = tempNum / 1000;
-			part = tempNum % 1000;
-			index++;
-			// Add extra space if next part is not zero
-			if (part > 0 && !words.equals("")) {
-				words = " and " + words;
-			}
-		} while (tempNum > 0);
+				// Add extra space if next part is not zero
+				if (((i+1) < parts.size()) && (parts[i+1] > 0)) {
+					words = " and " + words;
+				}
+			}	
+		}
+		
+		
+		
+// 		int part = positiveNum % 1000;
+// 		int tempNum = positiveNum;
+		
+// 		int index = 0;
+
+// 		do {
+// 			if (part > 0) {
+// 				words = NumToWordUtility.toWordsFromThreeDigits(part) + NumToWordConstants.MILESTONES[index]
+// 						+ words;
+// 			}
+// 			tempNum = tempNum / 1000;
+// 			part = tempNum % 1000;
+// 			index++;
+// 			// Add extra space if next part is not zero
+// 			if (part > 0 && !words.equals("")) {
+// 				words = " and " + words;
+// 			}
+// 		} while (tempNum > 0);
 
 		if (isNegative) {
 			words = "(" + words + ")";
 		}
 		return words;
+	}
+	
+	private void validateInput(final long num) {
+		if (num < -999_999_999 || num > 999_999_999) {
+			throw new ExceedRangeException(
+					"Invalid input! Give input between -999999999 to 999999999 inclusive.");
+		}
 	}
 }
